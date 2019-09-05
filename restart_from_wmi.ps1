099c7a51533d32a60fa7ec15011be8bcf55b7b32
Konstantin Grahovsky <grahovsky@gmail.com>
2019-09-05 09:57:26 +0300

    fix script

--- restart_from_wmi.ps1
+++ restart_from_wmi.ps1
@@ -2,15 +2,15 @@
 
 $server1c = 'localhost'
 $agentPort = '1540'
-$serviceName = '1C:Enterprise*'
+$serviceName = '1C:Enterprise\ 8\.3\ Server\ Agent\ \(x86-64\)'
+$comObjectName = 'V83.ComConnector'
 
 function getWorkingProcessPID {
    
    $SrvAddr = $server1c + ":$agentPort"
-   $ComObject = 'V83.ComConnector'
 
    try {
-      $V83Com = New-Object -ComObject V83.COMConnector
+      $V83Com = New-Object -ComObject $comObjectName
       $ServerAgent = $V83Com.ConnectAgent($SrvAddr)
    }
    catch {
@@ -36,45 +36,35 @@ function getWorkingProcessPID {
 
 function stopService {
    
-   $remote_services = (Get-WmiObject -Class Win32_Service -ComputerName $server1c | Where-Object { $_.Name -match $serviceName -and $_.State -eq 'Running' })
+   $remote_service = (Get-WmiObject -Class Win32_Service -ComputerName $server1c | Where-Object { $_.Name -match $serviceName -and $_.State -eq 'Running' })
 
-   foreach ($remote_service in $remote_services) {
-   
-      $pidProcArray = getWorkingProcessPID
-       
-      $remote_service.StopService()
-      sleep 10
+   $pidProcArray = getWorkingProcessPID
+      
+   $remote_service.StopService()
+   sleep 10
 
-      $remote_process = (Get-WmiObject -Class Win32_Process -ComputerName $server1c | Where-Object { $pidProcArray -contains $_.ProcessID })
-      foreach ($remote_proces in $remote_process) {
-         $win_proces.Terminate
-      }
-  
+   $remote_processes = (Get-WmiObject -Class Win32_Process -ComputerName $server1c | Where-Object { $pidProcArray -contains $_.ProcessID })
+   foreach ($remote_process in $remote_processes) {
+      $win_process.Terminate
    }
 
-   $remote_services = (Get-WmiObject -Class Win32_Service -ComputerName $server1c | Where-Object { $_.Name -match $serviceName -and $_.State -ne 'Stopped' })
+   $remote_service = (Get-WmiObject -Class Win32_Service -ComputerName $server1c | Where-Object { $_.Name -match $serviceName -and $_.State -ne 'Stopped' })
 
-   foreach ($remote_service in $remote_services) {
-      
-      $remote_pid = $remote_service.ProcessID    
-      $remote_process = (Get-WmiObject -Class Win32_Process -ComputerName $server1c | Where-Object { $_.ProcessID -eq $remote_pid })
-      foreach ($remote_proces in $remote_process) {
-         $remote_proces.Terminate
-      }
-      
+   $remote_pid = $remote_service.ProcessID    
+   $remote_processes = (Get-WmiObject -Class Win32_Process -ComputerName $server1c | Where-Object { $_.ProcessID -eq $remote_pid })
+   foreach ($remote_process in $remote_processes) {
+      $remote_process.Terminate
    }
-
+      
 }
 
 function startService {
 
-   $remote_services = (Get-WmiObject -Class Win32_Service -ComputerName $server1c | Where-Object { $_.Name -match $serviceName })
+   $remote_service = (Get-WmiObject -Class Win32_Service -ComputerName $server1c | Where-Object { $_.Name -match $serviceName })
 
-   foreach ($remote_service in $remote_services) {
-      if ($remote_service.State -ne 'Running') {
-         $remote_service.StartService()
-         sleep 20
-      }
+   if ($remote_service.State -ne 'Running') {
+      $remote_service.StartService()
+      sleep 20
    }
 
 }
